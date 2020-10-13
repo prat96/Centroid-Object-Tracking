a42e36fb6b3f5eac97029fb07aeb113d00713d92
prat96 <pratheekb96@gmail.com>
2020-10-13 19:43:13 +0200

    Add maxDistance logic

--- src/centroidtracker.cpp
+++ src/centroidtracker.cpp
@@ -132,6 +132,10 @@ std::vector<std::pair<int, std::pair<int, int>>> CentroidTracker::update(vector<
         for (int i = 0; i < rows.size(); i++) {
             //if we have already examined either the row or column value before, ignore it
             if (usedRows.count(rows[i]) || usedCols.count(cols[i])) { continue; }
+
+            // Added maxDistance logic here
+            if (Distances[rows[i]][cols[i]] > this->maxDistance) { continue; }
+           
             //otherwise, grab the object ID for the current row, set its new centroid,
             // and reset the disappeared counter
             int objectID = objectIDs[rows[i]];
